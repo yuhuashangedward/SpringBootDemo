16bea6a4ac50238f20b0f461c6798d56c1bfac39
yuhuashang <yuhuashang@aliyun.cfom>
2018-04-17 17:53:28 +0800

    SpringBoot中定时任务的实现

--- src/main/java/com/alibaba/job/ScheduledTasks.java
+++ src/main/java/com/alibaba/job/ScheduledTasks.java
@@ -11,7 +11,7 @@ public class ScheduledTasks {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(fixedRate = 10000)
     public void reportCurrentTime(){
         System.out.println("现在时间：" + dateFormat.format(new Date()));
     }
